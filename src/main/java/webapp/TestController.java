d500de78243d3c1f1c5587254dd435d1c1567341
LAPTOP-O6394742\Daniel <vdaniel@mail.upb.de>
2018-11-19 09:36:53 +0100

    security & jpa

--- src/main/java/webapp/TestController.java
+++ src/main/java/webapp/TestController.java
@@ -1,6 +1,8 @@
 package webapp;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,6 +12,11 @@ public class TestController {
     public ModelAndView index() {
         ModelAndView model=new ModelAndView("index");
         model.addObject("message","view Message");
+        return model;
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView model=new ModelAndView("login");
         return model;
     }
 }
--- src/main/java/webapp/conf/MvcConfiguration.java
+++ src/main/java/webapp/conf/MvcConfiguration.java
@@ -1,22 +1,27 @@
 package webapp.conf;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan
 public class MvcConfiguration implements WebMvcConfigurer {
     @Bean
     public ViewResolver getViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/");
         resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
         return resolver;
     }
     @Override
@@ -25,5 +30,9 @@ public class MvcConfiguration implements WebMvcConfigurer {
         //registry.addResourceHandler("/webResources/**").addResourceLocations("classpath:webResources/");
         //registry.addResourceHandler("/dataId/corpora/**").addResourceLocations("classpath:dataId/corpora/");
         //registry.addResourceHandler("/dataId/annotators/**").addResourceLocations("classpath:dataId/annotators/");
+    }
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
